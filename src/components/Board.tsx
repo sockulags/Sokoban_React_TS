59db6980dd4074f720963eb43fd7c805dc981be6
Lucas Skog <lucasskog@gmail.com>
2024-03-12 09:51:19 +0100

    Refactored handleKeyDown

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import Tile from './Tile';
-import { level1, level1Layout } from '../data/levels';
-import './board.css';
+import React, { useState, useEffect } from "react";
+import Tile from "./Tile";
+import { level1, level1Layout } from "../data/levels";
+import "./board.css";
 
 interface Position {
   x: number;
@@ -12,81 +12,81 @@ const Board = () => {
   const [board, setBoard] = useState<number[][]>(level1);
   const [charPos, setCharPos] = useState<Position | undefined>();
 
-const getCharStartPosition = () => {
-    const posY = board.findIndex(row=> row.includes(5));
-    console.log(posY)
-    const posX = board[posY].findIndex(x=> x === 5);
-    console.log(posX)
-    return {x: posX, y: posY}
-}
+  const getCharStartPosition = () => {
+    const posY = board.findIndex((row) => row.includes(5));
+    console.log(posY);
+    const posX = board[posY].findIndex((x) => x === 5);
+    console.log(posX);
+    return { x: posX, y: posY };
+  };
 
-useEffect(() => {
+  useEffect(() => {
     const startPosition = getCharStartPosition();
     setCharPos(startPosition);
-  }, );
+  }, []);
 
-
-  const upDateCharPos = (y:number, x:number) => {
+  const upDateCharPos = (y: number, x: number) => {
     const newBoard = [...board];
     newBoard[charPos!.y][charPos!.x] = 3;
     newBoard[y][x] = 5;
-    setCharPos({ y:y ,x:x });
+    setCharPos({ y: y, x: x });
     setBoard(newBoard);
-  }
+  };
 
-  const boxMove = (y:number, x:number) =>{
+  const boxMove = (y: number, x: number) => {
     const newBoard = [...board];
-     newBoard[y][x] = 2;
-     setBoard(newBoard);
-  }
+    newBoard[y][x] = 2;
+    setBoard(newBoard);
+  };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    let newY: number = 0;
+    let newX: number = 0;
+    let newBoxPositionX: number = 0;
+    let newBoxPositionY: number = 0;
     if (charPos)
-    switch (event.key){
+      switch (event.key) {
         case "ArrowUp":
-            if (board[charPos.y -1][charPos.x] === 3){
-                upDateCharPos(charPos.y - 1, charPos.x)
-            } else if (board[charPos.y -1][charPos.x] === 2){
-                if (board[charPos.y -2][charPos.x] === 3){
-                    boxMove(charPos.y - 2, charPos.x)
-                    upDateCharPos(charPos.y - 1, charPos.x)
-                }
-            }
-            break;
+          newY = charPos.y - 1;
+          newBoxPositionY = newY - 1;
+          newX = charPos.x;
+          newBoxPositionX = newX;
+          break;
         case "ArrowDown":
-            if (board[charPos.y + 1][charPos.x] === 3) {
-                upDateCharPos(charPos.y + 1, charPos.x);
-            }    else if (board[charPos.y + 1][charPos.x] === 2){
-                if (board[charPos.y +2][charPos.x] === 3){
-                    boxMove(charPos.y + 2, charPos.x)
-                    upDateCharPos(charPos.y + 1, charPos.x)
-                }
-            }
-            break;
+          newY = charPos.y + 1;
+          newBoxPositionY = newY + 1;
+          newX = charPos.x;
+          newBoxPositionX = newX;
+          break;
         case "ArrowLeft":
-            if (board[charPos.y][charPos.x - 1] === 3) {
-                upDateCharPos(charPos.y, charPos.x -1);
-            }    else if (board[charPos.y ][charPos.x -1] === 2){
-                if (board[charPos.y][charPos.x - 2] === 3){
-                    boxMove(charPos.y, charPos.x -2)
-                    upDateCharPos(charPos.y, charPos.x -1)
-                }
-            }
-            break;
+          newY = charPos.y;
+          newBoxPositionY = newY;
+          newX = charPos.x - 1;
+          newBoxPositionX = newX - 1;
+          break;
         case "ArrowRight":
-            if (board[charPos.y][charPos.x + 1] === 3) {
-                upDateCharPos(charPos.y, charPos.x + 1);
-            }    else if (board[charPos.y ][charPos.x +1] === 2){
-                if (board[charPos.y][charPos.x + 2] === 3){
-                    boxMove(charPos.y, charPos.x +2)
-                    upDateCharPos(charPos.y, charPos.x +1)
-                }
-            }
-            break;
-            default:
-                return;       
-    }
+          newY = charPos.y;
+          newBoxPositionY = newY;
+          newX = charPos.x + 1;
+          newBoxPositionX = newX + 1;
+          break;
+      }
 
+    switch (board[newY][newX]) {
+      case 3:
+      case 4:
+        upDateCharPos(newY, newX);
+        break;
+      case 2:
+        if (
+          board[newBoxPositionY][newBoxPositionX] === 3 ||
+          board[newBoxPositionY][newBoxPositionX] === 4
+        ) {
+          boxMove(newBoxPositionY, newBoxPositionX);
+          upDateCharPos(newY, newX);
+        }
+        break;
+    }
   };
 
   return (
@@ -94,10 +94,7 @@ useEffect(() => {
       {level1.map((row, rowIndex) => (
         <div className="row" key={rowIndex}>
           {row.map((tile, colInd) => (
-            <Tile
-              key={`${rowIndex}-${colInd}`}
-              image={level1Layout[tile]}
-            />
+            <Tile key={`${rowIndex}-${colInd}`} image={level1Layout[tile]} />
           ))}
         </div>
       ))}
