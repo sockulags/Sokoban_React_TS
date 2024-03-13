1b84f45a763c957a4aaabe9e0575195c41c3fa5b
Lucas Skog <lucasskog@gmail.com>
2024-03-13 11:02:46 +0100

    Added onclick events and created an object to map out each navbar item

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,43 @@
 import './Navbar.css';
 import Logo from "../assets/sokoban-header.png"
 
+interface NavbarRoutes{
+    id: number;
+    name: string;
+    path: string;
+}
+
+const routes:NavbarRoutes[] = [{
+    id: 1,
+    name: "Home",
+    path: "/",
+},{
+    id: 2,
+    name: "About",
+    path: "/about",
+},
+{
+    id: 3,
+    name: "Highscore",
+    path: "/highscore",
+}
+]
+
 export const Navbar = () => {
+
+    const handleClick = (path:string) => {
+        console.log("Try to navigate to " + path)
+    }
+
     return (
         <div className='navbar-container'>
            <div className="logo-container">
             <img src={Logo}/>
            </div>
            <div className="navbar-links">
-           <h2>Home</h2>
-           <h2>Highscores</h2>
-           <h2>How To Play</h2>
+{routes.map(route => {
+    return <h2 key={route.id} onClick={() => handleClick(route.path)}>{route.name}</h2>
+})}
            </div>
         </div>
     );
