ba5e4f7cca0495c46aae26a3389df241c49b1160
Therese Weidenstedt <kw222mi@student.lnu.se>
2024-03-26 10:24:52 +0100

    add listener for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -15,21 +16,36 @@ import GamePlay from "./pages/GamePlay";
 
 export function App() {
   const location = useLocation();
-  const hideNavbar = location.pathname.includes("/play");
- 
+  const windowRef = useRef(window); // Create a referens to window object
+  const [isMobile, setIsMobile] = useState(false);
+
+    const handleResize = useCallback(() => {
+      setIsMobile(windowRef.current.innerWidth < 768);
+    }, [setIsMobile]);
+
+ useEffect(() => {
+   handleResize();
+   windowRef.current.addEventListener("resize", handleResize);
+
+   return () => {
+     // eslint-disable-next-line react-hooks/exhaustive-deps
+     windowRef.current.removeEventListener("resize", handleResize);
+   };
+ }, [handleResize]);
+
+  const hideNavbar = /^\/play\/\d+$/.test(location.pathname) && isMobile;
+
   return (
-   
-      <div>
-        {!hideNavbar && <Navbar />}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/play" element={<Play />} />
-          <Route path="/play/:id" element={<GamePlay />} />
-          <Route path="/highscore/:id?" element={<HighscorePage />} />
-          {/* Definiera andra routes här */}
-        </Routes>
-      </div>
-   
+    <div>
+      {!hideNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/play" element={<Play />} />
+        <Route path="/play/:id" element={<GamePlay />} />
+        <Route path="/highscore/:id?" element={<HighscorePage />} />
+        {/* Definiera andra routes här */}
+      </Routes>
+    </div>
   );
 }
