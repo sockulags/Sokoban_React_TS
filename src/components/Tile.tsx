28c342ee936c1ccf6933b27a7268c2573b403282
Lucas Skog <lucasskog@gmail.com>
2024-04-02 16:39:03 +0200

    Create levels almost doone

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -4,14 +4,21 @@ type Props = {
   getTileImage: (rowIndex: number, colIndex: number) => string;
   rowIndex: number;
   colIndex: number;
+  onClick?: (rowIndex: number, colIndex: number) => void;
 };
 
-const Tile = ({ getTileImage, rowIndex, colIndex }: Props) => {
+const Tile = ({ getTileImage, rowIndex, colIndex, onClick }: Props) => {
   const image = getTileImage(rowIndex, colIndex);
 
+  const handleOnClick = () => {
+    if(onClick)
+    onClick(rowIndex, colIndex);
+  console.log("Tile Clicked")
+  }
+
   return (
-    <div className="tile">
-      <img src={image} />
+    <div className="tile" onClick={handleOnClick} >
+      <img src={image} onClick={() => handleOnClick}/>
     </div>
   );
 };
